c6d983454424a4d300ac0de5a2c9531e1380a12e
joxquinnn <joxquinnn1137@gmail.com>
2026-04-30 15:46:32 -0400

    feat: implementacion funcionalidad suma y resta

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,19 @@ public class Main {
         int a = 0;
         int b = 0;
 
+        double resultado = 0;
         // Base para el ejercicio: este switch debe ser trabajado en varias feature/*.
         // El conflicto intencional se genera cuando dos o mas features editan las mismas lineas.
         switch (opcion) {
             case 1:
                 System.out.println("TODO: implementar suma");
+                resultado = a + b;
+                System.out.println("el resultado es:" + resultado);
                 break;
             case 2:
                 System.out.println("TODO: implementar resta");
+                resultado = a - b;
+                System.out.println("el resultado es:" + resultado);
                 break;
             case 3:
                 System.out.println("TODO: implementar multiplicacion");
